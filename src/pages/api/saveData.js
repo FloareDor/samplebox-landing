2fdd316be87d1cb892d4ab6f9d0276e9b4191b30
Ra <ravicity999@gmail.com>
2024-07-12 04:59:24 +0530

    chore: removed geolocation

--- src/pages/api/saveData.js
+++ src/pages/api/saveData.js
@@ -15,14 +15,16 @@ export default async function handler(req, res) {
       const ip = forwardedFor ? forwardedFor.split(',')[0] : req.socket.remoteAddress;
 
       // Im using geoip-lite to get location info
-	  const geo = geoip.lookup(ip);
-      const location = geo ? {
-        country: geo.country,
-        continent: geo.continent,
-        region: geo.region,
-        city: geo.city,
-        timezone: geo.timezone
-      } : null;
+	//   const geo = geoip.lookup(ip);
+    //   const location = geo ? {
+    //     country: geo.country,
+    //     continent: geo.continent,
+    //     region: geo.region,
+    //     city: geo.city,
+    //     timezone: geo.timezone
+		//   } : null;
+		const location = null;
+	  
 
       await saveEmail(email, location);
       res.status(201).json({ message: 'Email saved successfully!' });
